fde7a827862486f8c7bb44f6cf90446c38db3bd6
Charisse <charisse.ysabel930@gmail.com>
2020-06-09 19:36:20 +0200

    Button disabled styles are not used

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -14,18 +14,15 @@ type ButtonProps = {
   children: React.ReactNode;
   type: "button" | "submit" | "reset";
   kind: "DEFAULT" | "PRIMARY";
-  disabled: boolean;
   onClick?: any;
 };
-const Button = ({ children, type, disabled, onClick, kind }: ButtonProps) => {
+const Button = ({ children, type, onClick, kind }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={className(styles.button, styles.default, {
-        [styles.disabled]: disabled,
+      className={className(styles.button, {
         [styles.primary]: kind === "PRIMARY",
       })}
-      disabled={disabled}
       onClick={onClick}
     >
       {children}
@@ -35,7 +32,6 @@ const Button = ({ children, type, disabled, onClick, kind }: ButtonProps) => {
 
 Button.defaultProps = {
   type: "button",
-  disabled: false,
   kind: "DEFAULT",
 };
 
