6633ced23eb7eda93259c9c1962fd580c4e9365b
Charisse <charisse.ysabel930@gmail.com>
2020-06-09 17:30:59 +0200

    Update data when item is deleted

--- frontend/src/pages/ServiceList/index.tsx
+++ frontend/src/pages/ServiceList/index.tsx
@@ -1,50 +1,32 @@
 import moment from "moment";
-import React, { useEffect, useState } from "react";
-import { deleteService, getAllServices } from "../../api/service";
+import React from "react";
+import { deleteService } from "../../api/service";
 import { Card } from "../../components/Card";
-import { ServiceDto } from "../../types";
-import styles from "./styles.module.css";
+import usePoller from "../../hooks/usePoller";
 import List from "./List";
-
-const INITIAL_VALUES = {
-  lastUpdate: "",
-  services: [],
-};
+import styles from "./styles.module.css";
 
 const ServiceList = () => {
-  const [serviceDto, setServiceDto] = useState<ServiceDto>(INITIAL_VALUES);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { data, isLoading, setData } = usePoller();
+  const { services, lastUpdate } = data;
 
-  const { services } = serviceDto;
-
-  useEffect(() => {
-    getAllServices().then(
+  const onDelete = (id: string) => {
+    deleteService(id).then(
       (res) => {
-        setIsLoading(false);
-        setServiceDto(res);
+        setData(res);
       },
       (e) => {
         throw new Error(e);
       }
-    );
-  }, []);
-
-  const onDelete = (id: string) => {
-    deleteService(id).then(
-      () => {
-        const updatedDto = serviceDto.services.filter((s) => s.id !== id);
-        setServiceDto((prev) => ({ ...prev, services: updatedDto }));
-      },
-      () => console.log("delete fail")
     );
   };
 
   return (
     <Card>
       <h1>Services List</h1>
-      {serviceDto.lastUpdate && (
+      {lastUpdate && (
         <small className={styles.lastUpdate}>
-          Updated {moment().calendar(serviceDto.lastUpdate)}
+          Updated {moment().calendar(lastUpdate)}
         </small>
       )}
 
