50c0a0283cf67844dd7f10349eacbbcad34c3582
Charisse <charisse.ysabel930@gmail.com>
2020-06-09 14:03:13 +0200

    Create AlertBox

---
+++ frontend/src/components/AlertBox/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import className from "classnames";
+import styles from "./styles.module.css";
+
+type AlertBox = {
+  message?: string;
+  type: "ERROR" | "SUCCESS";
+  children?: React.ReactNode;
+};
+
+const AlertBox = ({ message, type, children }: AlertBox) => {
+  return (
+    <div
+      className={className(styles.box, {
+        [styles.error]: type === "ERROR",
+        [styles.success]: type === "SUCCESS",
+      })}
+    >
+      {message ? message : children}
+    </div>
+  );
+};
+export default AlertBox;
