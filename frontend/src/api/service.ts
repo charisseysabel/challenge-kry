f51f3b4414924fb48f00ac44c55352d87e707671
Charisse <charisse.ysabel930@gmail.com>
2020-06-09 19:30:51 +0200

    Remove extra put action in the api

--- frontend/src/api/service.ts
+++ frontend/src/api/service.ts
@@ -1,32 +1,27 @@
 import axios from "axios";
-import { BaseService, Service, ServiceDto } from "../types";
+import { BaseService, ServiceDto } from "../types";
 
-const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:8080" : "";
+const URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:8080/service" : "";
 
 export const getAllServices = async (): Promise<ServiceDto> => {
-  const res = await axios.get(`${BASE_URL}/service`);
+  const res = await axios.get(`${URL}`);
   return await res.data;
 };
 
 export const addService = async (service: BaseService): Promise<any> => {
-  const res = await axios.post(`${BASE_URL}/service`, { data: service });
-  return res.data;
-};
-
-export const editService = async (service: Service): Promise<any> => {
-  const res = await axios.put(`${BASE_URL}/service`, service);
+  const res = await axios.post(`${URL}`, { data: service });
   return res.data;
 };
 
 export const deleteService = async (serviceId: string): Promise<ServiceDto> => {
-  const res = await axios.delete(`${BASE_URL}/service`, {
+  const res = await axios.delete(`${URL}`, {
     data: { id: serviceId },
   });
   return res.data;
 };
 
 export const updateService = async (service: BaseService): Promise<any> => {
-  const res = await axios.put(`${BASE_URL}/service`, service);
+  const res = await axios.put(`${URL}`, service);
   return res.data;
 };
